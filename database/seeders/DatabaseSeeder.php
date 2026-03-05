dbd65cd6d67d9308ab0cc3e20fb1f2ba7ffcf9bc
rahel-yab <rahelyabebal@gmail.com>
2026-03-05 19:47:32 +0300

    Update databaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,9 +17,18 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        \App\Models\User::create([
+        'name' => 'Manager User',
+        'email' => 'manager@test.com',
+        'password' => bcrypt('password'),
+        'role' => 'manager',
+    ]);
+
+    \App\Models\User::create([
+        'name' => 'Dev User',
+        'email' => 'dev@test.com',
+        'password' => bcrypt('password'),
+        'role' => 'developer',
+    ]);
     }
 }
