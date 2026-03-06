5c6c3d93c23f273ca91896732c5fc5e8afc99eaa
rahel-yab <rahelyabebal@gmail.com>
2026-03-06 08:35:51 +0300

    added user and task relationship to project

---
+++ app/Models/Project.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Project extends Model
+{
+    protected $fillable = ['name', 'description', 'created_by', 'deadline', 'status'];
+
+    public function creator(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'created_by');
+    }
+
+    public function tasks(): HasMany
+    {
+        return $this->hasMany(Task::class);
+    }
+}
